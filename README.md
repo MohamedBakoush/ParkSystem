71bacf01cc8b89d77e1ce58ea227a05ea6e204aa
Max Hutchings <up892525@myport.ac.uk>
2020-02-24 14:49:17 +0000

    Implement Server-Client
    -Moved the scripts for maps.html into a seperate javascript file
    -Moved the files for the client into a seperate client folder
    -Set up the package.json with dependecies
    -Set up the base server code
    -Updated README.md to include how to set up the website to run on port 8080 of personal machines

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ParkSystem
 
 ParkSystem - Software Engineering Project
+
+# Setting Up The Website
+* Clone the repository using 'git clone https://github.com/MohamedBakoush/ParkSystem.git'
+* Navigate to the directory on your personal machine through the terminal using 'cd <Insert Directory>'
+* Install all of the dependencies for the website by running 'npm i'
+* Run the website on your local host port 8080 by running 'npm start'
+* Using your chosen web browser go to 'http://localhost:8080/' to view the website
---
+++ client/index.js
@@ -0,0 +1,100 @@
+let map;
+// create google maps
+function createMap() {
+  let location = {
+    lat: 50.7952,
+    lng: -1.0872
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 15,
+    mapTypeId: "roadmap"
+  });
+  // to requrest only parking sapce and the radius it covers
+  let requestParkingSpaces = {
+    location: map.getCenter(),
+    radius: 8000,
+    types: ['parking']
+  }
+  let service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(requestParkingSpaces, callback);
+}
+// if staus is ok callback takes each available parking space in area and puts them into specified functions
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      // takes all places (parking spaces) available and puts into each function
+      createMarker(results[i]);
+      fetchParkingInfo(results[i]);
+    }
+    console.log("PlacesServiceStatus OK");
+  } else {
+    console.log("PlacesServiceStatus Not OK");
+  }
+}
+
+// creats a marker on google map
+function createMarker(place) {
+  // creats a marker
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    title: place.name,
+    url: place.id,
+    label: "P",
+  });
+  // link to place ticket page
+  const placeInfolink = window.location.href + "/place=" + place.place_id;
+  // content string which will be inputed into infowindow in google maps
+  const contentString = '<div> <strong>' + place.name + '</strong><br>' +
+    'Place ID: ' + place.place_id + '<br>' +
+    'Address : ' + place.vicinity + '<br>' +
+    'Opening Hours : ' + place.opening_hours + '<br>' +
+    'Rateing : ' + place.rating + '<br>' +
+    `<a href='${placeInfolink}'> Place Information </a>`;
+  let infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  // opens infowindow when click on marker
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+// fetches parking information and puts into mapListContainer element
+function fetchParkingInfo(place) {
+  const mapListContainer = document.getElementById('mapListContainer');
+
+  // creates a div in mapListContainer which is already defined in html code
+  let ParkingInfo = document.createElement('div');
+  ParkingInfo.id = "ParkingInfo";
+  mapListContainer.appendChild(ParkingInfo);
+
+  // Parking Info Container for containg parking Information
+  let ParkingInfoContainerInfo = document.createElement('div');
+  ParkingInfoContainerInfo.id = 'ParkingInfoContainerInfo';
+  ParkingInfo.appendChild(ParkingInfoContainerInfo);
+
+  // name of parking space inside in ParkingInfoContainerInfo
+  let ParkingInfoName = document.createElement('div');
+  ParkingInfoName.id = 'ParkingInfoName';
+  ParkingInfoName.textContent = place.name;
+  ParkingInfoContainerInfo.appendChild(ParkingInfoName);
+
+  // Address of parking space inside in ParkingInfoContainerInfo
+  let ParkingInfoAddress = document.createElement('div');
+  ParkingInfoAddress.id = 'ParkingInfoAddress';
+  ParkingInfoAddress.textContent = place.vicinity;
+  ParkingInfoContainerInfo.appendChild(ParkingInfoAddress);
+
+  // Parking Info Cost Container Containg parking cost
+  let ParkingInfoContainerCost = document.createElement('div');
+  ParkingInfoContainerCost.id = 'ParkingInfoContainerCost';
+  ParkingInfo.appendChild(ParkingInfoContainerCost);
+
+  // Cost of parking space inside in ParkingInfoContainerCost
+  let ParkingInfoCost = document.createElement('div');
+  ParkingInfoCost.id = 'ParkingInfoCost';
+  ParkingInfoCost.textContent = 'ParkingInfoCost';
+  ParkingInfoContainerCost.appendChild(ParkingInfoCost);
+}
