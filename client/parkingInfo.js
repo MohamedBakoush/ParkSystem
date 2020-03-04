373dba7283d34727b2921038e4df0bb8ba73e251
Mohamed Bakoush <bakoushmohamed@gmail.com>
2020-03-04 02:00:34 +0000

    loadCostDetails for various options
    Made prev ticket available depending if opening hours exists as 24hr, specified hours and Not available for opneing hours that dosent exist.
    
    to make this happen i had to move the required inputs into js and adjust them depending on the scenario
    
    added header to ParkingInfo.html
    
    check comments for more detail on what everything dose

--- client/parkingInfo.js
+++ client/parkingInfo.js
@@ -39,7 +39,7 @@ function getParkingInfomation(){
 
     } catch (e) {
       // Opening Info Not Available
-      data(ParkingTimeContainer, "Opening Hours Not Available", "ParkingTime_openingHours_");
+      data(ParkingTimeContainer, "Opening Hours Not Available", "ParkingTime_openingHours");
     }
 
 
@@ -101,8 +101,158 @@ async function loadParkingDetail() {
   showParkingDetail_Info(parkingDetail);
 }
 
+
+async function loadCostDetails(){
+  const service = new google.maps.places.PlacesService(map);
+  const request = {
+    placeId: getParkingDetail_Id(),
+    // type of values to get from
+    fields: ['opening_hours','opening_hours.weekday_text']
+    };
+  service.getDetails(request, function(place) {
+    // get current Date and Hour/Min
+    const date = new Date();
+    const currentDate = date.toISOString().slice(0,10);
+    currentHour = checkTime(date.getHours());
+    currentHMinute = checkTime(date.getMinutes());
+    const currentTime = `${currentHour}:${currentHMinute}`;
+
+    // gets Date and depending on the date it will update
+    // todays date accordingly
+   let todaysDate;
+    if(date.getDay() == 1){ todaysDate = 0;
+    } else if(date.getDay() == 2) { todaysDate = 1;
+    } else if(date.getDay() == 3) { todaysDate = 2;
+    } else if(date.getDay() == 4) { todaysDate = 3;
+    } else if(date.getDay() == 5) { todaysDate = 4;
+    } else if(date.getDay() == 6) { todaysDate = 5;
+    } else if(date.getDay() == 7) { todaysDate = 6;
+    }
+    try {
+      const opening_hours = place.opening_hours.weekday_text;
+      // splits up the array opening_hours
+      const openSplit = opening_hours[todaysDate].split(" ");
+      if(openSplit[2] == 24){ // Opening Hours: Open 24 Hours
+        console.log(" Open 24 Hours");
+        const checkIn = document.getElementById("checkIn");
+        createLabel(checkIn, "Check-In");
+        ticketDataDate(checkIn, "date", currentDate, false);
+
+        const checkOut = document.getElementById("checkOut");
+        createLabel(checkOut, "Check-Out");
+        ticketDataDate(checkOut, "date", currentDate, false);
+
+        const timeIn = document.getElementById("timeIn");
+        createLabel(timeIn, "Time-In");
+        ticketDataTime(timeIn, "time", "00:00", "24:00", "00:00", false);
+
+        const timeOut = document.getElementById("timeOut");
+        createLabel(timeOut, "Time-Out");
+        ticketDataTime(timeOut, "time", "00:00", "24:00", "23:59", false);
+
+        const prevTicket_btn = document.getElementById("prevTicket_btn");
+        createprevTicket(prevTicket_btn, "submit", "Preview Ticket", false);
+
+      } else {  // Opening Hours: Open specific Hours
+        console.log("Open specific Hours");
+        const opening_hours = place.opening_hours.weekday_text;
+        const openSplit = opening_hours[todaysDate].split(" ");
+        const splitOpenTime = openSplit[1].split(":");
+        const openStringTime_Open24hr  =  `0${splitOpenTime[0]}:${splitOpenTime[1]}`;
+        const splitCloseTime = openSplit[4].split(":");
+        const openStringTime_close24hr  =  `${ Number(splitCloseTime[0]) + 12}:${splitCloseTime[1]}`;
+
+        const checkIn = document.getElementById("checkIn");
+        createLabel(checkIn, "Check-In");
+        ticketDataDate(checkIn, "date", currentDate, false);
+
+        const checkOut = document.getElementById("checkOut");
+        createLabel(checkOut, "Check-Out");
+        ticketDataDate(checkOut, "date", currentDate, false);
+
+        const timeIn = document.getElementById("timeIn");
+        createLabel(timeIn, "Time-In");
+        ticketDataTime(timeIn, "time", openStringTime_Open24hr, openStringTime_close24hr, openStringTime_Open24hr, false);
+
+        const timeOut = document.getElementById("timeOut");
+        createLabel(timeOut, "Time-Out");
+        ticketDataTime(timeOut, "time", openStringTime_Open24hr, openStringTime_close24hr, openStringTime_close24hr, false);
+
+        const prevTicket_btn = document.getElementById("prevTicket_btn");
+        createprevTicket(prevTicket_btn, "submit", "Preview Ticket", false);
+      }
+
+    } catch (e) { // this is here if Open hour dont exist
+
+      console.log("Open hour dont exist");
+      const checkIn = document.getElementById("checkIn");
+      createLabel(checkIn, "Check-In");
+      ticketDataDate(checkIn, "date", currentDate, true);
+
+      const checkOut = document.getElementById("checkOut");
+      createLabel(checkOut, "Check-Out");
+      ticketDataDate(checkOut, "date", currentDate, true);
+
+      const timeIn = document.getElementById("timeIn");
+      createLabel(timeIn, "Time-In");
+      ticketDataTime(timeIn, "time", "00:00", "00:00", "00:00", true);
+
+      const timeOut = document.getElementById("timeOut");
+      createLabel(timeOut, "Time-Out");
+      ticketDataTime(timeOut, "time", "00:00", "00:00", "00:00", true);
+
+      const prevTicket_btn = document.getElementById("prevTicket_btn");
+      createprevTicket(prevTicket_btn, "submit", "Preview Ticket", true);
+    }
+  });
+
+}
+// Creats button for prev Ticket
+function createprevTicket(container, type, value, disabled){
+  const button = document.createElement('input');
+  button.type = type;
+  button.value = value;
+  button.disabled = disabled;
+  container.appendChild(button);
+}
+// label maker
+function createLabel(container, string){
+  const label = document.createElement('label');
+  label.textContent = string;
+  container.appendChild(label);
+}
+// Creats input section for ticket Date
+function ticketDataDate(container, type, value, disabled){
+  const input = document.createElement('input');
+  input.type = type;
+  input.value = value;
+  input.required = "true";
+  input.disabled = disabled;
+  container.appendChild(input);
+}
+// Creats input section for ticket time
+function ticketDataTime(container, type, open, close, displayTime, disabled){
+  const input = document.createElement('input');
+  input.type = type;
+  input.min = open;
+  input.max = close;
+  input.value = displayTime;
+  input.required = "true";
+  input.disabled = disabled;
+  container.appendChild(input);
+}
+// if time is less the then 10 it will show as a singel didget
+// to avoid show as a double didget this function adds a 0 before the i value
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
 function pageLoaded() {
   loadParkingDetail();
+  loadCostDetails();
 }
 
 window.addEventListener('load', pageLoaded);
