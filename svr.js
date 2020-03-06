65945b6483a3f07c21eb8a4e0ce6d492bce9a27b
Mohamed Bakoush <bakoushmohamed@gmail.com>
2020-03-06 19:14:40 +0000

    changed how get lat/lon to show up in maps
    ~ changed the way to collect lat/lon by removing the database which we used to store lan/lon in to takeing lat/lon from herf intsed on maps.html

--- svr.js
+++ svr.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const pib = require('./parkingInfoBoard');
-const latlon = require('./lanlog');
 
 // adds html as extensions
 // dont need to write index.html
@@ -13,12 +12,6 @@ function getParkingDetails(request, response){
   //gets all Parking Details available
   response.json(pib.listParkingDetails());
 }
-
-function getLatLonDetails(request, response){
-  //gets all Lat Lon Details available
-  response.json(latlon.listLatLonDetails());
-}
-
 
 
 function getParkingDetail(req, res){
@@ -40,21 +33,9 @@ function postParkingDetail(req, res) {
   res.json(parkingDetails);
 }
 
-// post Lat Lon into LatLonDetails (Lat and Lon database)
-function postLatLonDetail(req, res) {
-  const LatLonDetails = latlon.addLatLonDetails(req.body.Latitude, req.body.Longitude);
-  res.json(LatLonDetails);
-}
-
-
 // for patking detail (mainly cost)
 app.get('/parkingDetails', getParkingDetails);
 app.get('/parkingDetails/:id', getParkingDetail);
 app.post('/parkingDetails', express.json(), postParkingDetail);
-
-// for Latitude and Longitude info
-app.get('/LatLonDetails', getLatLonDetails);
-app.post('/LatLonDetails', express.json(), postLatLonDetail);
-
 
 app.listen(8080);
