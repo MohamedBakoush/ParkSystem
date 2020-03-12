9daf970d7ecec38284f9973b573d7f6ee60d6312
Mohamed Bakoush <bakoushmohamed@gmail.com>
2020-03-12 14:48:31 +0000

    header
    -header.js
    --rewrote functions to be more modular
    -styleHeader.css
    --made header style fit new header structure
    -changed html to fit new header

--- client/javaScript/header.js
+++ client/javaScript/header.js
@@ -1,23 +1,38 @@
+
 function showHeader_Info() {
-  const nav = document.getElementById('headerNav');
-  const logoLinks = document.getElementById('logoLinks');
-  const signinLinks = document.getElementById('signinLinks')
-  headerContainer(logoLinks,"li", "a", "Logo" , "Logo", "ParkSystem", "/");
-  headerContainer(signinLinks,"li", "a", "login" , "login", "login", "#");
-  headerContainer(signinLinks,"li", "a", "register" , "register", "Register", "#");
+  const container = getById("headerNav");
+  const logoContainer = makeElement(container, "section", "logoLinks", "logoLinks");
+  contentInfo(logoContainer, "a", "Logo", "Logo", "ParkSystem", "/");
+
+  const accInfoContainer = makeElement(container, "ul", "signinLinks", "signinLinks");
+  const accInfoRegister = makeElement(accInfoContainer, "li");
+  const accInfoLogin = makeElement(accInfoContainer, "li");
+  contentInfo(accInfoLogin, "a", "login", "login", "login", "#");
+  contentInfo(accInfoRegister, "a", "register", "register", "register", "#");
 }
 
-
-function headerContainer(container, type1, type2, classHere , idHere, textContent, href){
-  const item = document.createElement(type1);
-  container.appendChild(item);
-  const aa = document.createElement(type2);
-  aa.classList = classHere;
-  aa.id = idHere;
-  aa.href = href;
-  aa.textContent  = textContent;
-  item.appendChild(aa);
+function getById(idName) {
+  const container = document.getElementById(idName);
+  return container;
 }
+
+function makeElement(container, elementTypeList, classHere, idHere){
+  const elementType = document.createElement(elementTypeList);
+  elementType.classList = classHere;
+  elementType.id = idHere;
+  container.appendChild(elementType);
+  return elementType;
+}
+
+function contentInfo(container, elementType, classHere , idHere, textContent, href){
+  const element = document.createElement(elementType);
+  element.classList = classHere;
+  element.id = idHere;
+  element.href = href;
+  element.textContent  = textContent;
+  container.appendChild(element);
+}
+
 function pageLoaded() {
   showHeader_Info();
 }
