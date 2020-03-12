8a165a045d0e0f29d4c407c3b9153449239f0a3e
Mohamed Bakoush <bakoushmohamed@gmail.com>
2020-03-12 03:11:40 +0000

    ticket test
    -ticket.js
    --rewrote functions to be more modular
    --exported all function
    -ticket.test.js
    --tested all functions except
    ---getById, loadParkingDetail, getTicketInfo

--- client/javaScript/ticket.js
+++ client/javaScript/ticket.js
@@ -1,59 +1,71 @@
 'use strict';
-const getTicketInfo = async (parkingDetail) => {
-  // speperates the herf into diffrent sections
-  // parking id, date , check in time and check out time
- const splitTicket = window.location.href.split("ticket",2);
- const splitCheck = splitTicket[1].split("?id=");
- const split_date_time = splitCheck[1].split("&time=");
- const split_id_date = split_date_time[0].split("&date=");
- const parkingID = split_id_date[0];
- const checkInDate = split_id_date[1];
- const checkInTime = split_date_time[1];
- const split_time_checkIn_HourMin = checkInTime.split("%3A");
- const checkOutTime = split_date_time[2];
- const split_time_checkOut_HourMin = checkOutTime.split("%3A");
- // calculates timeDiffrenceHour
- let timeDiffrenceHour = split_time_checkOut_HourMin[0] - split_time_checkIn_HourMin[0];
-  // calculates timeDiffrenceMin
- let timeDiffrenceMin = split_time_checkOut_HourMin[1] - split_time_checkIn_HourMin[1];
 
- if (timeDiffrenceMin < 0) {
-   // if number timeDiffrenceMin is negative then reverse the check in and check out to get a possitive value then
-   //  - by 60  (hour is 60 sec)
-   // timeDiffrenceHour - 1  = to take in consideration for min change
-   timeDiffrenceMin = split_time_checkIn_HourMin[1] - split_time_checkOut_HourMin[1];
-   timeDiffrenceMin = 60 - timeDiffrenceMin;
-   timeDiffrenceHour = timeDiffrenceHour - 1;
- }
- if (timeDiffrenceMin < 10) {
-   // checkTIme - if timeDiffrenceMin is singel diget then add 0 infromt of number
-  timeDiffrenceMin = checkTime(timeDiffrenceMin);
- }
+function splitHref() {
+  const splitTicket = window.location.href.split("ticket",2);
+  const splitCheck = splitTicket[1].split("?id=");
+  const split_date_time = splitCheck[1].split("&time=");
+  const split_id_date = split_date_time[0].split("&date=");
+  const parkingID = split_id_date[0];
+  const checkInDate = split_id_date[1];
+  const checkInTime = split_date_time[1];
+  const split_time_checkIn_HourMin = checkInTime.split("%3A");
+  const checkInHour = split_time_checkIn_HourMin[0];
+  const checkInMin = split_time_checkIn_HourMin[1];
+  const checkOutTime = split_date_time[2];
+  const split_time_checkOut_HourMin = checkOutTime.split("%3A");
+  const checkOutHour = split_time_checkOut_HourMin[0];
+  const checkOutMin = split_time_checkOut_HourMin[1];
+  return[parkingID, checkInDate, checkInHour, checkInMin, checkOutHour, checkOutMin]
+}
 
-  if (timeDiffrenceHour < 0) {
-    // when timeDiffrenceHour is negative show error
-    timeDiffrenceHour = "TotalTime";
-    timeDiffrenceMin = "Error";
-  } else if (timeDiffrenceHour < 10) {
-    // checkTime - if timeDiffrenceHour is singel diget then add 0 infront of number
-   timeDiffrenceHour = checkTime(timeDiffrenceHour);
-   }
- // add timeCheckIn Hour,Min together
- const  timeCheckIn = `${split_time_checkIn_HourMin[0]}:${split_time_checkIn_HourMin[1]}`;
- // add timeCheckOut Hour,Min together
- const  timeCheckOut = `${split_time_checkOut_HourMin[0]}:${split_time_checkOut_HourMin[1]}`;
- // add totalTimeDiffrence Hour,Min together
- const totalTimeDiffrence = `${timeDiffrenceHour}:${timeDiffrenceMin}`;
+function calculateTime(checkInHour, checkInMin, checkOutHour, checkOutMin ) {
+  // calculate time hout/min diffrence
+  let timeDiffrenceHour = checkOutHour - checkInHour;
+  let timeDiffrenceMin = checkOutMin - checkInMin;
 
+  if (timeDiffrenceMin < 0) {
+    timeDiffrenceMin = checkInMin - checkOutMin; // if number timeDiffrenceMin is negative then reverse the check in and check out to get a possitive value then
+    timeDiffrenceMin = 60 - timeDiffrenceMin;     //  - by 60  (hour is 60 sec)
+    timeDiffrenceHour = timeDiffrenceHour - 1;     // timeDiffrenceHour - 1  = to take in consideration for min change
+    }else {
+     timeDiffrenceMin = checkTime(timeDiffrenceMin); // checkTIme - if timeDiffrenceMin is singel diget then add 0 infromt of number
+    }
+
+   if (timeDiffrenceHour < 0) { // when timeDiffrenceHour is negative show error
+     timeDiffrenceHour = "TotalTime";
+     timeDiffrenceMin = "Error";
+   } else{  // checkTime - if timeDiffrenceHour is singel diget then add 0 infront of number
+    timeDiffrenceHour = checkTime(timeDiffrenceHour);
+    }
+  // add Hour,Min together
+  const timeCheckIn = `${checkInHour}:${checkInMin}`;
+  const timeCheckOut = `${checkOutHour}:${checkOutMin}`;
+  const totalTimeDiffrence = `${timeDiffrenceHour}:${timeDiffrenceMin}`;
+
+  return [timeCheckIn, timeCheckOut, totalTimeDiffrence, timeDiffrenceHour, timeDiffrenceMin];
+}
+
+function getById(idName) {
+  const container = document.getElementById(idName);
+  return container;
+}
+
+function costData(container, costCurrency, costContent, createElementType, classHere, idHere) {
+  if (isNaN(costContent) == false) { //if cost is not a number then
+    ticketData(container, `${costCurrency}${costContent}`, createElementType, classHere, idHere);
+    return "costContent is not Null";
+  } else{ // if cost is a number
+    ticketData(container, `Parking Cost Not Available`, createElementType, classHere, idHere);
+    return "costContent is Null";
+  }
+}
+
+function calculateCost(parkingDetail, timeDiffrenceHour, timeDiffrenceMin) {
   let cost = 0;
   if(timeDiffrenceHour >= 1){ // when timeDiffrenceHour is 1 or greater add initial cost
-    console.log("pay 1 hour");
     cost = cost + parkingDetail.cost1Hour;
-    console.log("cost:", cost);
     if (timeDiffrenceHour < 2) { // if timeDiffrenceHour is lest then 2 hours but still greater then 1 hour then only add 1 additional hour to the initial cost
-          console.log("additional hours");
-          cost = cost + parkingDetail.costAdditionalHour;
-          console.log("cost:", cost);
+      cost = cost + parkingDetail.costAdditionalHour;
     }else{ // depending on timeDiffrenceHour - inital hour add as additional hours
       cost = cost + (parkingDetail.costAdditionalHour * (timeDiffrenceHour - 1))
       if(timeDiffrenceMin > 1){ // if there is a time diffrence in min then add an additional hour
@@ -69,29 +81,9 @@ const getTicketInfo = async (parkingDetail) => {
       cost = parkingDetail.cost1Hour;
     }
   }
-
- // get element id from ticket.html
-  const CostContainer = document.getElementById("parkingCostFinal");
-  const TimeCheckInContainer = document.getElementById("TimeCheckIn");
-  const TimeCheckOutContainer = document.getElementById("TimeCheckOut");
-  const TimeDiffrenceContainer = document.getElementById("TimeDiffrence");
-  const DateDiffrenceContainer = document.getElementById("DateDiffrence");
- // shows ticket data in html
-  ticketData(TimeCheckInContainer, `${timeCheckIn}`, "p", "timeCheckIn", "timeCheckIn");
-  ticketData(TimeCheckOutContainer, `${timeCheckOut}`, "p", "timeCheckOut", "timeCheckOut");
-  ticketData(TimeDiffrenceContainer, `${totalTimeDiffrence}`, "p", "timeDiffrence", "timeDiffrence");
-  ticketData(DateDiffrenceContainer, `${checkInDate}`, "p", "dateDiffrence", "dateDiffrence");
- // get buy ticket id from ticket.html
-  const buyTicket_btn = document.getElementById("buyTicket_btn");
-  // shows BuyTicket button in html
-  createBuyTicket(buyTicket_btn, "submit", "Buy Ticket", true, "buyTicket", "buyTicket");
-
-  if (isNaN(cost) == false) { //if cosy is not a number then
-    ticketData(CostContainer, `£${cost}`, "p", "finalCost", "finalCost");
-  } else{ // if cost is a number
-    ticketData(CostContainer, `Parking Cost Not Available`, "p", "finalCost", "finalCost");
-  }
+  return cost;
 }
+
 // shows BuyTicket button in html
 function createBuyTicket(container, type, value, disabled, classHere, idHere){
   const button = document.createElement('input');
@@ -101,34 +93,36 @@ function createBuyTicket(container, type, value, disabled, classHere, idHere){
   button.id = idHere;
   button.disabled = disabled;
   container.appendChild(button);
+  return button;
 }
 
 // shows ticket data in html
-function  ticketData(container, textContent, datatype, classHere, idHere){
+function ticketData(container, textContent, datatype, classHere, idHere){
   const ticketData = document.createElement(datatype);
   ticketData.textContent = textContent;
   ticketData.classList = classHere;
   ticketData.id = idHere;
   container.appendChild(ticketData);
+  return ticketData;
 }
 
 // if time is less the then 10 it will show as a singel didget
 //  this function adds a 0 before the i value to show time as a double didget
-const checkTime = (i) => {
-  if (i < 10) {
-    i = "0" + i;
+function checkTime(number) {
+  if (number < 10) {
+    number = "0" + number;
   }
-  return i;
+  return number;
 }
 
-const getParkingDetail_Id = () => {
+function getParkingDetail_Id() {
   const splitHerf = window.location.href.split("ticket?id=",2);
   const split_id_date = splitHerf[1].split("&");
   const parkingID = split_id_date[0];
   return parkingID;
 };
 
-const loadParkingDetail = async() => {
+async function loadParkingDetail() {
   const id = getParkingDetail_Id();
   const response = await fetch(`parkingDetails/${id}`);
   let parkingDetail;
@@ -138,12 +132,47 @@ const loadParkingDetail = async() => {
     parkingDetail = { msg: 'failed to load messages' };
   }
   // show parking details
-
   getTicketInfo(parkingDetail);
+  return parkingDetail;
+}
+
+function getTicketInfo(parkingDetail) {
+  // speperates the herf into diffrent sections
+  // parking id, date , check in time and check out time
+  const ticketHrefData = splitHref();
+  const parkingID = ticketHrefData[0];
+  const checkInDate = ticketHrefData[1];
+  const checkInHour = ticketHrefData[2];
+  const checkInMin = ticketHrefData[3];
+  const checkOutHour = ticketHrefData[4];
+  const checkOutMin = ticketHrefData[5];
+
+  const time = calculateTime(checkInHour, checkInMin, checkOutHour, checkOutMin);
+  const timeCheckIn = time[0];
+  const timeCheckOut = time[1];
+  const totalTimeDiffrence = time[2];
+  const timeDiffrenceHour = time[3];
+  const timeDiffrenceMin = time[4];
+
+  const cost = calculateCost(parkingDetail, timeDiffrenceHour, timeDiffrenceMin );
+
+  // get element id from ticket.html
+  const timeCheckInContainer = getById("TimeCheckIn");
+  const timeCheckOutContainer = getById("TimeCheckOut");
+  const timeDiffrenceContainer = getById("TimeDiffrence");
+  const dateDiffrenceContainer = getById("DateDiffrence");
+  const costContainer = getById("parkingCostFinal");
+  const buyTicket_btn = getById("buyTicket_btn");
+ // shows ticket data in html
+  ticketData(timeCheckInContainer, timeCheckIn, "p", "timeCheckIn", "timeCheckIn");
+  ticketData(timeCheckOutContainer, timeCheckOut, "p", "timeCheckOut", "timeCheckOut");
+  ticketData(timeDiffrenceContainer, totalTimeDiffrence, "p", "timeDiffrence", "timeDiffrence");
+  ticketData(dateDiffrenceContainer, checkInDate, "p", "dateDiffrence", "dateDiffrence");
+  costData(costContainer, "£", cost, "p", "finalCost", "finalCost");
+  createBuyTicket(buyTicket_btn, "submit", "Buy Ticket", true, "buyTicket", "buyTicket");
 }
 
 function pageLoaded() {
-   // getTicketInfo();
    loadParkingDetail();
 }
 
@@ -151,6 +180,15 @@ window.addEventListener('load', pageLoaded);
 
 module.exports = {
   // export modules
+  splitHref,
+  calculateTime,
+  getById,
+  costData,
+  calculateCost,
+  createBuyTicket,
+  ticketData,
   checkTime,
-  getParkingDetail_Id
+  getParkingDetail_Id,
+  loadParkingDetail,
+  getTicketInfo,
 };
--- client/javaScript/ticket.test.js
+++ client/javaScript/ticket.test.js
@@ -1,4 +1,8 @@
-const { checkTime, getParkingDetail_Id} = require('./ticket');
+'use strict'
+const {splitHref, calculateTime, getByI, costData,
+      calculateCost, createBuyTicket, ticketData,
+      checkTime,getParkingDetail_Id, loadParkingDetail,
+      getTicketInfo} = require('./ticket');
 global.window = Object.create(window);
 const url = "http://localhost:8080/ticket?id=ChIJ0XjfNHRddEgRQtXe1fjPW8w&date=2020-03-09&time=08%3A00&time=18%3A30";
 Object.defineProperty(window, 'location', {
@@ -7,14 +11,100 @@ Object.defineProperty(window, 'location', {
   }
 });
 
-test('check Time if < 10, if yes add 0 infront of value', () => {
-  const time1 = checkTime(1);
-  expect(time1).toBe("01");
-  const time2 = checkTime(27);
-  expect(time2).toBe(27);
-});
+const {findParkingDetail} = require('../../parkingInfoBoard');
 
-test('check if ID gets corrent value from url', () => {
-  const id = getParkingDetail_Id();
-  expect(id).toBe("ChIJ0XjfNHRddEgRQtXe1fjPW8w");
-});
+describe('ticket', function () {
+  const herf = splitHref();
+  const parkingID = herf[0];
+  const checkInDate = herf[1];
+  const checkInHour = herf[2];
+  const checkInMin = herf[3];
+  const checkOutHour = herf[4];
+  const checkOutMin = herf[5];
+  it('check splitHref', () => {
+    expect(herf).toBeDefined();
+    expect(parkingID).toBe("ChIJ0XjfNHRddEgRQtXe1fjPW8w");
+    expect(checkInDate).toBe("2020-03-09");
+    expect(checkInHour).toBe("08");
+    expect(checkInMin).toBe("00");
+    expect(checkOutHour).toBe("18");
+    expect(checkOutMin).toBe("30");
+  });
+  const time = calculateTime(checkInHour, checkInMin, checkOutHour, checkOutMin);
+  const timeCheckIn = time[0];
+  const timeCheckOut = time[1];
+  const totalTimeDiffrence = time[2];
+  const timeDiffrenceHour = time[3];
+  const timeDiffrenceMin = time[4];
+  it('check calculateTime', () => {
+    expect(time).toBeDefined();
+    expect(timeCheckIn).toBe("08:00");
+    expect(timeCheckOut).toBe("18:30");
+    expect(totalTimeDiffrence).toBe("10:30");
+    expect(timeDiffrenceHour).toBe(10);
+    expect(timeDiffrenceMin).toBe(30);
+  });
+
+  // TODO: getById
+  // it('check getById', () => {
+  // });
+
+  const parkingDetail = findParkingDetail("ChIJ0XjfNHRddEgRQtXe1fjPW8w");   // to get data from the database id - ChIJ0XjfNHRddEgRQtXe1fjPW8w
+  const cost = calculateCost(parkingDetail, timeDiffrenceHour, timeDiffrenceMin);
+  it('check calculateCost', () => {
+    expect(cost).toBeDefined();
+    expect(cost).toBe(13);;
+  });
+
+  const createElement = document.createElement("section");
+  it('check costData', () => {
+    const dataCost = costData(createElement, "£", cost,  "p", "finalCost", "finalCost")
+    expect(dataCost).toBe("costContent is not Null");
+    expect(dataCost).toBeDefined();
+    const noDataCost = costData(createElement, "£", "cost",  "p", "finalCost", "finalCost")
+    expect(noDataCost).toBe("costContent is Null");
+    expect(noDataCost).toBeDefined();
+  });
+  it('check createBuyTicket', () => {
+    const buyTicket = createBuyTicket(createElement, "submit", "Buy Ticket", true, "buyTicket", "buyTicket");
+    expect(buyTicket).toBeDefined();
+    expect(buyTicket.type).toBe("submit");
+    expect(buyTicket.value).toBe("Buy Ticket");
+    expect(buyTicket.classList[0]).toBe("buyTicket");
+    expect(buyTicket.id).toBe("buyTicket");
+    expect(buyTicket.disabled).toBe(true);
+  });
+
+  it('check ticketData', () => {
+    const dataTicket = ticketData(createElement, `textContent`, "p", "time_Class", "time_ID");
+    expect(dataTicket).toBeDefined();
+    expect(dataTicket.textContent).toBe("textContent");
+    expect(dataTicket.classList[0]).toBe("time_Class");
+    expect(dataTicket.id).toBe("time_ID");
+  });
+
+  it('check checkTime', () => {
+    const timeLess10 = checkTime(1);
+    expect(timeLess10).toBeDefined();
+    expect(timeLess10).toBe("01");
+    expect(typeof timeLess10).toBe("string");
+    const time10OrMore = checkTime(27);
+    expect(time10OrMore).toBeDefined();
+    expect(time10OrMore).toBe(27);
+    expect(typeof time10OrMore).toBe("number");
+  });
+  it('check getParkingDetail_Id', () => {
+    const id = getParkingDetail_Id();
+    expect(id).toBeDefined();
+    expect(id).toBe("ChIJ0XjfNHRddEgRQtXe1fjPW8w");
+    expect(typeof id).toBe("string");
+  });
+
+  // TODO: loadParkingDetail
+  // it('check loadParkingDetail', () => {
+  // });
+
+  // TODO: getTicketInfo
+  // it('check getTicketInfo', () => {
+  // });
+})
