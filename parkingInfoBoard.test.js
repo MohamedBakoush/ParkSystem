b358b99c970856739ab5828cdab870dc293a5ebe
Mohamed Bakoush <bakoushmohamed@gmail.com>
2020-03-15 03:47:04 +0000

    Update parkingInfoBoard.test.js
    added more values to test for in findParkingDetail

--- parkingInfoBoard.test.js
+++ parkingInfoBoard.test.js
@@ -10,6 +10,8 @@ test('Check if findParkingDetail can bring data from specified id', () => {
   //id_example the id which is available on parkingDetails
   const find = findParkingDetail("id_example");
   expect(find.id).toBe('id_example');
-  expect(find.name).toBe('Parking_Info_Name');
+  expect(find.cost15Min).toBe('Parking_Cost_15_Min');
+  expect(find.cost30Min).toBe('Parking_Cost_30_Min');
+  expect(find.cost1Hour).toBe('Parking_Cost_1_Hour');
+  expect(find.costAdditionalHour).toBe('Parking_Cost_Additional_Hour');
 });
- 
